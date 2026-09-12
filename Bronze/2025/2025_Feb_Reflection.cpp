9f7e83fbeb2d1b75ea4661dd0c9a72107ebb0af6
norby <norbert.cheng.lu@outlook.com>
2026-09-12 12:53:58 -0700

    one more

--- Bronze/2025/2025_Feb_Reflection.cpp
+++ Bronze/2025/2025_Feb_Reflection.cpp
@@ -1,14 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() 
+int main()
 {
-    int N,U;
+
+    int N, U;
     cin >> N >> U;
-    vector<int> cows(N);
-    for (int i = 0;i<N;i++)
+    vector<string> cows(N);
+    vector<vector<int>> count(N / 2, vector<int>(N / 2, 0));
+
+    for (int i = 0; i < N; i++)
     {
         cin >> cows[i];
+        for (int j = 0; j < N; j++)
+        {
+            if (cows[i][j] == '#')
+            {
+                count[min(i, N- 1 - i)][min(j, N - 1 - j)]++;
+            }
+        }
     }
-    
+
+    int ans = 0;
+    for (int i = 0; i < N / 2; i++)
+    {
+        for (int j = 0; j < N / 2; j++)
+        {
+            ans += min(count[i][j], 4 - count[i][j]);
+        }
+    }
+    cout << ans << '\n';
+
+    for (int i = 0; i < U; i++)
+    {
+        int r, c;
+        cin >> r >> c;
+        r--;
+        c--;
+
+        int a = min(r, N - 1 - r);
+        int b = min(c, N - 1 - c);
+        ans -= min(count[a][b], 4 - count[a][b]);
+
+        if (cows[r][c] == '#')
+        {
+            cows[r][c] = '.';
+            count[a][b]--;
+        }
+        else
+        {
+            cows[r][c] = '#';
+            count[a][b]++;
+        }
+
+        ans += min(count[a][b], 4 - count[a][b]);
+        cout << ans << '\n';
+    }
 }
